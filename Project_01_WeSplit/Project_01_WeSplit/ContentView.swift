369a18f1df5006e80136280150c9505f2966dd2a
sardaarNiamotullah <sardaar.niamotullah@gmail.com>
2026-03-18 00:12:31 +0600

    Fix: convert submodule to normal project

---
+++ Project_01_WeSplit/Project_01_WeSplit/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Project_01_WeSplit
+//
+//  Created by Sardaar Niamotullah on 17/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(code: "USD")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(code: "USD")
+                    )
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_01_WeSplit/Project_01_WeSplit/Project_01_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_01_WeSplitApp.swift
+//  Project_01_WeSplit
+//
+//  Created by Sardaar Niamotullah on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Project_01_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
